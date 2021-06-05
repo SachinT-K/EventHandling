75d48fd55be65d9464f240be95bf4b31c15a7eba
SachinT-K <75876472+SachinT-K@users.noreply.github.com>
2021-06-05 20:29:45 +0530

    React forms and capturing changing events.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 
 function App() {
   const [isMousedOver, setMousedOver] = useState(false);
-  const [headingText, setHeadingText] = useState("Hello");
+  const [headingText, setHeadingText] = useState("");
+  const [name, setName] = useState("");
 
   function handleMouseOver() {
     setMousedOver(true);
@@ -12,22 +13,34 @@ function App() {
     setMousedOver(false);
   }
 
-  function handleClick() {
-    setHeadingText("Submitted");
+  function handleClick(event) {
+    setHeadingText(name);
+    event.preventDefault();
+  }
+
+  function handleChange(event) {
+    setName(event.target.value);
   }
 
   return (
     <div className="container">
-      <h1>{headingText}</h1>
-      <input type="text" placeholder="What's your name?" />
-      <button
-        style={{ backgroundColor: isMousedOver ? "black" : "white" }}
-        onMouseOver={handleMouseOver}
-        onMouseOut={handleMouseOut}
-        onClick={handleClick}
-      >
-        Submit
-      </button>
+      <h1>Hello {headingText}</h1>
+      <form action="" onSubmit={handleClick}>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="What's your name?"
+          value={name}
+        />
+        <button
+          type="submit"
+          style={{ backgroundColor: isMousedOver ? "black" : "white" }}
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
+        >
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
